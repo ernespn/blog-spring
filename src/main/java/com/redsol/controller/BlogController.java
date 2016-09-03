d0f7dab3da9fb1bc9c6b1d264d5e6d345101fa3c
Ernesto Perez <ernespn@gmail.com>
2016-09-03 13:17:03 +0100

    Initial setup and hello world example

---
+++ src/main/java/com/redsol/controller/BlogController.java
@@ -0,0 +1,17 @@
+package com.redsol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BlogController {
+	
+	@RequestMapping(value="/greeting")
+	public String sayHello (Model model){
+		
+		model.addAttribute("greeting", "Hello World");
+		
+		return "hello";
+	}
+}
